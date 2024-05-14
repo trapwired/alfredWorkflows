fcc3b57127381316c66eda9388ec443cfec327a7
Dominic Weibel <dominic.weibel@digitecgalaxus.ch>
2024-05-14 15:49:40 +0200

    refactor and fix bugs

--- webnotes/main.py
+++ webnotes/main.py
@@ -108,10 +108,6 @@ def find(names, path):
                 return Path(new_path).relative_to(path).as_posix()
 
 
-def get_fullpath(filename):
-    return os.path.join(PATH, filename)
-
-
 def is_jira(filename):
     return (JIRA_PATH != 'SAMPLE/JIRA/TEMPLATE/PATH'
             and JIRA_FOLDER_NAME != 'SAMPLE_JIRA_FOLDER_NAME'
@@ -151,59 +147,9 @@ def find_jira(issue_number, path, filename):
                 return Path(new_path).relative_to(path).as_posix()
 
 
-def get_or_create_file(url: str, website_title: str):
-    filename = get_filename(website_title)
-    filename_fullpath = get_fullpath(filename)
-    index = get_index()
-
-    if url not in index.keys():
-        filename = create_new_file(filename, filename_fullpath, website_title, url)
-
-    else:
-        if not os.path.exists(filename_fullpath):
-            # file was either renamed, moved or deleted
-            old_file_path = os.path.join(PATH, index[url])
-            if os.path.exists(old_file_path):
-                # file was renamed, rename at original location
-                head, tail = os.path.split(old_file_path)
-                new_path = os.path.join(head, filename)
-                os.rename(old_file_path, new_path)
-            else:
-                # file was moved or deleted
-                filenames = [index[url], filename]
-                found_filename = find_file(filename, filenames)
-                filename = create_new_if_not_found(filename, filename_fullpath, found_filename, website_title, url)
-
-    index[url] = filename
-    write_index(index)
-    correct_filename = index[url]
-    return correct_filename
-
-
-def find_file(filename, filenames):
-    template = is_template(filename)
-    match template:
-        case TEMPLATE.JIRA:
-            issue_number = get_issue_number(filename)
-            found_filename = find_jira(issue_number, PATH, filename)
-        case _:
-            found_filename = find(filenames, PATH)
-    return found_filename
-
-
-def create_new_if_not_found(filename, filename_fullpath, found_filename, website_title, url):
-    if found_filename is None:
-        # file was deleted, create new one
-        filename = create_new_file(filename, filename_fullpath, website_title, url)
-    else:
-        filename = found_filename
-    return filename
-
-
-def create_new_file(filename, filename_fullpath, website_title, url):
-    while os.path.exists(filename_fullpath):
+def create_new_file(filename, website_title, url):
+    while os.path.exists(os.path.join(PATH, filename)):
         filename = website_title + str(randint(0, 100)) + FILE_EXTENSION
-        filename_fullpath = os.path.join(PATH, filename)
 
     template = is_template(filename)
     match template:
@@ -212,41 +158,9 @@ def create_new_file(filename, filename_fullpath, website_title, url):
         case TEMPLATE.JIRA_PR:
             return create_jira_pr_template(filename, url)
         case _:
-            with open(filename_fullpath, 'w') as file:
+            with open(os.path.join(PATH, filename), 'w') as file:
                 file.write(get_header_with_link(url))
             return filename
-
-
-def create_jira_template(filename, url):
-    filename = os.path.join(JIRA_FOLDER_NAME, filename)
-    jira_folder_path = os.path.join(PATH, filename)
-    shutil.copyfile(JIRA_PATH, jira_folder_path)
-
-    filename_fullpath = os.path.join(PATH, filename)
-    with open(filename_fullpath, 'r') as file:
-        filedata = file.read()
-
-    for key, value in jira_template_values(url).items():
-        filedata = filedata.replace(key, value)
-
-    with open(filename_fullpath, 'w') as file:
-        file.write(filedata)
-    return filename
-
-
-def get_jira_issue_link_from_pr_title(filename):
-    if 'noissue' in filename.lower():
-
-        return '#NOISSUE-PR'
-
-    for option in OPTIONS:
-        split_1 = filename.split(f'{option}-')
-        if len(split_1) == 1:
-            # we are not in this option
-            continue
-        split_2 = split_1[1].split(' ')
-        number = split_2[0]
-        return f'https://jiradg.atlassian.net/browse/{option}-{number}'
 
 
 def create_jira_pr_template(filename, url):
@@ -268,6 +182,37 @@ def create_jira_pr_template(filename, url):
     return filename
 
 
+def create_jira_template(filename, url):
+    filename = os.path.join(JIRA_FOLDER_NAME, filename)
+    jira_folder_path = os.path.join(PATH, filename)
+    shutil.copyfile(JIRA_PATH, jira_folder_path)
+
+    filename_fullpath = os.path.join(PATH, filename)
+    with open(filename_fullpath, 'r') as file:
+        filedata = file.read()
+
+    for key, value in jira_template_values(url).items():
+        filedata = filedata.replace(key, value)
+
+    with open(filename_fullpath, 'w') as file:
+        file.write(filedata)
+    return filename
+
+
+def get_jira_issue_link_from_pr_title(filename):
+    if 'noissue' in filename.lower():
+        return '#NOISSUE-PR'
+
+    for option in OPTIONS:
+        split_1 = filename.split(f'{option}-')
+        if len(split_1) == 1:
+            # we are not in this option
+            continue
+        split_2 = split_1[1].split(' ')
+        number = split_2[0]
+        return f'https://jiradg.atlassian.net/browse/{option}-{number}'
+
+
 def init_config(main_path: str):
     global PATH, JIRA_PATH, JIRA_FOLDER_NAME, JIRA_PR_PATH, JIRA_PR_FOLDER_NAME, INDEX_PATH
     config = configparser.RawConfigParser()
@@ -282,6 +227,55 @@ def init_config(main_path: str):
         INDEX_PATH = config.get('OPTIONS', 'index_path')
     except Exception:
         pass
+
+
+def get_or_create_file(url: str, website_title: str):
+    index = get_index()
+    filename = get_filename(website_title)
+
+    if url not in index.keys():
+        # url not yet indexed, create new file
+        filename = create_new_file(filename, website_title, url)
+
+    else:
+        # url indexed
+        full_file_path = os.path.join(PATH, filename)
+
+        # file exists? --> Happy flow
+        if not os.path.exists(full_file_path):
+            # file was either renamed, moved or deleted
+            old_file_path = os.path.join(PATH, index[url])
+            file_found = find([index[url], filename], PATH)
+            if os.path.exists(old_file_path):
+                # file was renamed, but stayed at original location
+                head, _ = os.path.split(old_file_path)
+                new_path = os.path.join(head, filename)
+                os.rename(old_file_path, new_path)
+
+            elif file_found:
+                # file was moved, update location in index
+                filename = file_found
+
+            else:
+                # file was moved and renamed or deleted
+
+                # maybe we can find the file cause its jira
+                issue_number = get_issue_number(filename)
+                found_jira_filename = find_jira(issue_number, PATH, filename)
+                if found_jira_filename:
+                    # rename jira issue at its current location
+                    original_full_path = os.path.join(PATH, found_jira_filename)
+                    head, _ = os.path.split(old_file_path)
+                    new_full_path = os.path.join(head, filename)
+                    os.rename(original_full_path, new_full_path)
+                else:
+                    # file must have been deleted, create new one
+                    filename = create_new_file(filename, website_title, url)
+
+    index[url] = filename
+    write_index(index)
+    correct_filename = index[url]
+    return correct_filename
 
 
 if __name__ == '__main__':
